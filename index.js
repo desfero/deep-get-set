de615b256a8d60b786d74ef8d3080526c37b49c5
Pavlo Liulia <pavloblack@hotmail.com>
2016-11-26 20:17:00 +0100

    update logic to handle array get and set

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
     var hasOwnProp = Object.prototype.hasOwnProperty;
 
     function deep (obj, path, value) {
-        if (arguments.length === 3) return set.apply(null, arguments);
-        return get.apply(null, arguments);
+        return (arguments.length === 3) ? set.apply(null, arguments) : get.apply(null, arguments);
     }
 
     function get (obj, path) {
-        var keys = path.split('.');
+        var keys = prepareKeys(path)
+                    .map(function(key) { return key.name });
         for (var i = 0; i < keys.length; i++) {
             var key = keys[i];
             if (!obj || !hasOwnProp.call(obj, key)) {
@@ -26,15 +26,64 @@
     }
 
     function set (obj, path, value) {
-        var keys = path.split('.');
-        for (var i = 0; i < keys.length - 1; i++) {
+        var keys = prepareKeys(path);
+
+        for(var i = 0; i < keys.length - 1; ++i) {
             var key = keys[i];
-            if (deep.p && !hasOwnProp.call(obj, key)) obj[key] = {};
-            obj = obj[key];
+
+            if(!hasOwnProp.call(obj, key.name)) {
+                obj[key.name] = (i < keys.length - 1) && keys[i+1].isArray ? [] : {};
+            }
+
+            obj = obj[key.name];
         }
-        obj[keys[i]] = value;
+
+        obj[keys[i].name] = value;
         return value;
     }
 
-  return deep;
+    function prepareKeys(path) {
+        return path.split('.').reduce(function (p, c) {
+            return p.concat(extractKey(c));
+        }, []);
+    }
+
+    function extractKey(key) {
+        var regExp = /([\d\w]*)\[(\d+)\]/g;
+        var match = regExp.exec(key), subMatch;
+
+        if (match !== null) {
+            var matchArray = match.slice(2).map(function(key) {
+                return {
+                    name: key,
+                    isArray: true
+                }
+            });
+
+            while ((subMatch = regExp.exec(key)) !== null) {
+                matchArray = matchArray.concat(subMatch.slice(2).map(function(key) {
+                    return {
+                        name: key,
+                        isArray: true
+                    }
+                }));
+            }
+
+            if (match[1]) {
+                matchArray.unshift({
+                    name: match[1],
+                    isArray: false
+                });
+            }
+
+            return matchArray;
+        }
+
+        return {
+            name: key,
+            isArray: false
+        };
+    }
+
+    return deep;
 })));
--- test.js
+++ test.js
@@ -2,47 +2,70 @@ var test = require('tape');
 var deep = require('./');
 
 test('deep gets', function (t) {
-  var obj = {
-    foo: 'bar',
-    bar: {
-      baz: {
-        beep: 'boop'
-      }
-    }
-  };
+    var obj = {
+        foo: 'bar',
+        bar: {
+            baz: {
+                beep: 'boop'
+            }
+        },
+        quz: ['yeey', { foo: 'yeah'}]
+    };
 
-  t.equal(deep(obj, 'foo'), 'bar');
-  t.equal(deep(obj, 'bar.baz.beep'), 'boop');
-  t.equal(deep(obj, 'bar.baz.beep.yep.nope'), undefined);
-  t.end();
+    t.equal(deep(obj, 'foo'), 'bar');
+    t.equal(deep(obj, 'bar.baz.beep'), 'boop');
+    t.equal(deep(obj, 'quz.0'), 'yeey');
+    t.equal(deep(obj, 'quz[1].foo'), 'yeah');
+    t.equal(deep(obj, 'bar.baz.beep.yep.nope'), undefined);
+    t.end();
 });
 
-test('deep sets', function (t) {
-  var obj = {
-    foo: 'bar',
-    bar: {
-      baz: {
-        beep: 'boop'
-      }
-    }
-  };
+test('deep sets on empty object', function (t) {
+    var obj = {};
+    t.equal(deep(obj, 'foo.bar', 'baz'), 'baz');
+    t.equal(obj.foo.bar, 'baz');
 
-  function p () {
-    deep(obj, 'yep.nope', 'p');
-  }
-  
-  t.equal(deep(obj, 'foo', 'yep'), 'yep');
-  t.equal(obj.foo, 'yep');
-  t.equal(deep(obj, 'bar.baz.beep', 'nope'), 'nope');
-  t.equal(obj.bar.baz.beep, 'nope');
-  t.throws(p);
+    obj = {};
+    t.equal(deep(obj, 'bar[4]', 'baz'), 'baz');
+    t.equal(Array.isArray(obj.bar), true);
+    t.equal(obj.bar[4], 'baz');
 
-  deep.p = true;
+    obj = {};
+    t.equal(deep(obj, 'bar[4][2]', 'baz'), 'baz');
+    t.equal(Array.isArray(obj.bar), true);
+    t.equal(Array.isArray(obj.bar[4]), true);
+    t.equal(obj.bar[4][2], 'baz');
 
-  t.equal(deep(obj, 'yep.nope', 'p'), 'p');
-  t.equal(obj.yep.nope, 'p');
+    obj = [];
+    t.equal(deep(obj, '[4][2]', 'baz'), 'baz');
+    t.equal(Array.isArray(obj[4]), true);
+    t.equal(obj[4][2], 'baz');
 
-  t.end();
+    t.end();
+});
+
+test('deep sets on existing object', function (t) {
+    var obj = {
+        bar: {
+            baz: 'foo'
+        },
+        foo: [1, 2],
+        quix: []
+
+    };
+
+    t.equal(deep(obj, 'bar.qux', 'quix'), 'quix');
+    t.equal(obj.bar.baz, 'foo');
+    t.equal(obj.bar.qux, 'quix');
+
+    t.equal(deep(obj, 'foo[2]', 3), 3);
+    t.equal(obj.foo[1], 2);
+    t.equal(obj.foo[2], 3);
+
+    t.equal(deep(obj, 'quix[1].bar', 'baz'), 'baz');
+    t.equal(obj.quix[1].bar, 'baz');
+
+    t.end();
 });
 
 test('deep deletes', function (t) {
